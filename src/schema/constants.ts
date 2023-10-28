68336be4704c3faedb69e8a4cae0b510af29dd7c
Greg Wardwell <gwardwell@wayfair.com>
2023-10-28 00:23:05 -0400

    feat: add additional constants

--- src/schema/constants.ts
+++ src/schema/constants.ts
@@ -1,3 +1,5 @@
+import {ConstDirectiveNode, Kind} from 'graphql';
+
 export const FED1_VERSION = 'v1';
 export const FED2_DEFAULT_VERSION = 'v2.0';
 export const FED2_VERSION_PREFIX = 'v2.';
@@ -8,3 +10,12 @@ export const EXTENDS_DIRECTIVE = 'extends';
 export const TAG_DIRECTIVE = 'tag';
 export const KEY_DIRECTIVE = 'key';
 export const INTERFACE_OBJECT_DIRECTIVE = 'interfaceObject';
+export const TYPENAME_FIELD_NAME = '__typename';
+export const CONTRACT_DIRECTIVE_NAME = 'tag';
+export const EXTERNAL_DIRECTIVE_AST = {
+  kind: Kind.DIRECTIVE,
+  name: {kind: Kind.NAME, value: EXTERNAL_DIRECTIVE},
+} as ConstDirectiveNode;
+export enum KeyDirectiveArgument {
+  Fields = 'fields',
+}
